3d62320ad7dcbff123b2ab6aa7d5e0944d01a6a3
Pooya Shirazi <p.shirazi.a@gmail.com>
2025-03-02 09:04:56 +0330

    ...

--- app.py
+++ app.py
@@ -1068,6 +1068,7 @@ async def send_message(message):
 
 @bot.message_handler(commands=["broadcast"])
 async def handle_broadcast(message):
+    print(message.from_user.id)
     if message.from_user.id != 7690029281:
         await bot.reply_to(message, "🚫 You are not authorized to use this command.")
         return
